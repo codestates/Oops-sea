d8fe4fc09776b2d53adffd6b11e98293eec8630a
SumSumCorn <a01035587180@gmail.com>
2022-04-14 13:55:24 +0900

    프론트 홈 디자인 전, 네브바폼 에러 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,68 @@
-import React, { useState } from "react";
 import "./App.css";
-import Home from "./Components/Home";
-import Explore from "./Components/Explore";
-import Create from "./Components/Create";
-import Account from "./Components/Account";
-import Navbar from "./Components/Navbar";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Explore from "./components/Explore";
+import Create from "./components/Create";
+import Account from "./components/Account";
+import Navbar from "./components/Navbar";
+import Web3 from "web3";
 
-const App = () => {
-	const [account, setAccount] = useState("");
+function App() {
+	// const [web3, setWeb3] = useState();
+	// const [account, setAccount] = useState('');
+	const [mainaccount, setMainaccount] = useState("");
+	const [mainweb3, setMainweb3] = useState();
+	const [clicked, setClicked] = useState();
+	const [isLogin, setIsLogin] = useState(false);
+	// const [newErc721addr, setNewErc721Addr] = useState('0x8dc27935bA6725025D4b96F49445392E7AE45c5B'); // my ERC721 CA
+	// const [erc721list, setErc721list] = useState([]);
+
+	useEffect(() => {
+		setMainaccount(mainaccount);
+	}, [mainaccount]);
+
+	useEffect(() => {
+		if (mainaccount === undefined || mainaccount === null) {
+			setIsLogin(false);
+		} else {
+			setIsLogin(true);
+		}
+	}, [mainaccount]);
+
+	const handleClicked = (e) => {
+		setClicked(e);
+	};
 
 	return (
 		<BrowserRouter>
-			<Navbar/>
-			<Routes>
-				<Route
-					exact
-					path="/"
-					element={<Home/>}
+			<div className="App">
+				<Navbar
+					setMainaccount={setMainaccount}
+					setMainweb3={setMainweb3}
+					isLogin={isLogin}
 				/>
-			</Routes>
-			<Routes>
-				<Route
-					path="/explore"
-					element={<Explore/>}
-				/>
-			</Routes>
-			<Routes>
-				<Route
-					path="/create"
-					element={<Create />}
-				/>
-			</Routes>
-			<Routes>
-				<Route
-					path="/account"
-					element={<Account/>}
-				/>
-			</Routes>
+				<Routes>
+					<Route
+						exact={true}
+						path="/"
+						element={<Home account={mainaccount} />}
+					/>
+					<Route
+						path="/explore"
+						element={
+							<Explore account={mainaccount} handleClicked={handleClicked} />
+						}
+					/>
+					<Route path="/create" element={<Create account={mainaccount} />} />
+					<Route
+						path="/account"
+						element={<Account web3={mainweb3} account={mainaccount} />}
+					/>
+				</Routes>
+			</div>
 		</BrowserRouter>
 	);
-};
+}
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+
 require('dotenv').config();
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
